45c92d835f756faaf1d4765fa3c64ea842c7c303
Victoriia Holub <mikimiki158@gmail.com>
2018-10-07 18:16:32 +0300

    Task 1 to first lesson

---
+++ Task1.py
@@ -0,0 +1,14 @@
+'''
+Write a script that can ask in users:
+'What is your name', 'How old are you', 
+'Where do you live'.
+'''
+print("What is your name?")
+name = input()
+print("Hello, " + name)
+print("How old are you?")
+age = input()   
+print("Your age is: " + age)
+print("Where do you live?")
+country = input()
+print("You live in " + country)
